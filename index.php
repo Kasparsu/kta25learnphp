d4d5c7a85a30a2485c81a9cf30754cb483f38b52
Kaspar Martin Suursalu <kasparsu@gmail.com>
2025-12-20 14:43:57 +0200

    Add functions examples

--- index.php
+++ index.php
@@ -1,40 +1,44 @@
 <?php
 
-for($i=0;$i<10;$i++) {
+function hello() {
+    var_dump('hello');
+}
+
+hello();
+
+function helloName($name='Nameless') {
+    var_dump("Hello $name!");
+}
+
+helloName('Kaspar');
+helloName('Martin');
+helloName();
+
+function sum($a, $b) {
+    return $a+$b;
+    var_dump('asdasd');
+}
+
+$answer = sum(2, 4);
+var_dump($answer);
+
+function values(...$values) {
+        var_dump(...$values);
+}
+
+values(1, 2, 3, 4, 5, 6);
+
+function name(string $name):string {
+    return ucwords($name);
+}
+
+//name(['asd']);
+
+function recursive($i) {
     var_dump($i);
+    if($i < 10){
+        recursive($i+1);
+    }
 }
 
-for($i=10;$i>0;$i--) {
-    var_dump($i);
-}
-
-
-for($i=1;$i<10_000_000;$i*=2) {
-    var_dump($i);
-}
-
-$time = time();
-var_dump($time);
-$i = 0;
-while($time+3 > time()) {
-    $i++;
-}
-var_dump($i);
-
-while(false) {
-    var_dump('WHILE');
-}
-
-do {
-    var_dump('DO');
-} while(false);
-
-$array = ['apple', 'cherry', 'pear'];
-
-foreach($array as $value) {
-    var_dump($value);
-}
-
-foreach($array as $i => $fruit) {
-    var_dump($i, $fruit);
-}
+recursive(0);
