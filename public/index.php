3729432c415fb621a70d3822aaea70ecdf257db8
Kaspar Martin Suursalu <kasparsu@gmail.com>
2026-01-08 09:58:38 +0200

    Add simple router

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+function dump(...$vars) {
+    echo '<pre>';
+    var_dump(...$vars);
+    echo '</pre>';
+}
+
+switch($_SERVER['REQUEST_URI']) {
+    case '/':
+        include '../views/index.php';
+        break;
+    case '/us':
+        include '../views/us.php';
+        break;
+    case '/tech':
+        include '../views/tech.php';
+        break;
+    default:
+        echo '404 - page not found';
+}
