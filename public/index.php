551c77651fbadf69bb0ae352f96b1f348824b9c7
Kaspar Martin Suursalu <kasparsu@gmail.com>
2026-01-08 10:45:35 +0200

    Add autoload and namespaces

--- public/index.php
+++ public/index.php
@@ -5,19 +5,36 @@ function dump(...$vars) {
     echo '</pre>';
 }
 
-switch($_SERVER['REQUEST_URI']) {
-    case '/':
-        $title = 'World News';
-        include '../views/page.php';
-        break;
-    case '/us':
-        $title = 'U.S News';
-        include '../views/page.php';
-        break;
-    case '/tech':
-        $title = 'Tech News';
-        include '../views/page.php';
-        break;
-    default:
-        echo '404 - page not found';
-}
+spl_autoload_register(function ($class) {
+    $class = str_replace('App\\', '', $class); 
+    require_once "../src/$class.php";
+});
+
+use App\Controllers\PublicController as PC;
+use App\Router;
+
+$router = new Router();
+dump($router);
+
+$db = new App\DB();
+dump($db);
+
+$controller = new PC();
+dump($controller);
+
+// switch($_SERVER['REQUEST_URI']) {
+//     case '/':
+//         $title = 'World News';
+//         include '../views/page.php';
+//         break;
+//     case '/us':
+//         $title = 'U.S News';
+//         include '../views/page.php';
+//         break;
+//     case '/tech':
+//         $title = 'Tech News';
+//         include '../views/page.php';
+//         break;
+//     default:
+//         echo '404 - page not found';
+// }
