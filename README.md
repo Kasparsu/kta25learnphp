85e32d998e5770cd9bf2a8a726e142dc3f984603
Kaspar Martin Suursalu <kasparsu@gmail.com>
2026-01-10 16:08:41 +0200

    Add posts crud

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # learnphp
+
+
+CRUD
+repe
+eadl
+adae
+t tt
+e ee
--- helpers.php
+++ helpers.php
@@ -10,3 +10,12 @@ function view($viewName, $variables=[]) {
     extract($variables);
     include __DIR__ . "/views/$viewName.php";
 }
+
+function dd(...$vars){
+    dump(...$vars);
+    die;
+}
+
+function redirect($path) {
+    header("Location: $path");
+}
